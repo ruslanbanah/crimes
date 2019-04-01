f12afba98a70be0235e87f295cba93c0f131e93a
Ruslan Banakh <ruslan.banakh@scoutgaminggroup.com>
2019-04-01 11:12:08 +0300

    Print out top 5 crime locations, each with list of associated theft incidents

--- src/main/scala/crime.scala
+++ src/main/scala/crime.scala
@@ -22,6 +22,11 @@ object start {
       events.groupBy(_.slice(0,2).mkString(",")).toSeq.sortBy(_._2.size).reverse.slice(0,num)
     }
 
+    def getThefts(row: List[List[String]]):List[String] = {
+      val pattern = "(T|t)heft".r
+      row.map(_.slice(2,3).toString).distinct.filter(s => pattern.findFirstIn(s).isDefined)
+    }
+
     def readAllFiles(absPath: String): List[List[String]] = {
       var result = List[List[String]]()
       val files = getListOfFiles(absPath)
@@ -36,7 +41,13 @@ object start {
     }
     val events = readAllFiles("/Users/ruslanbanah/work/tmp/scala_crimes/src/main/scala/crimes")
     println("Events count: ", events.length)
-    val topFive = top(events, 5)
-    println(topFive)
+    val results = top(events, 5)
+    results.foreach(row => {
+      val thefts = getThefts(row._2)
+      println("----------------------------")
+      println(s"(${row._1}): ${row._2.size}")
+      println("Thefts:")
+      thefts.foreach(s => println(s.toString))
+    })
   }
 }
