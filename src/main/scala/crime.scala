8f1fee6ef3519b2d7ea565c5bdf0d3e1ce407301
Ruslan Banakh <ruslan.banakh@scoutgaminggroup.com>
2019-04-03 12:59:10 +0300

    fix sort, add lat,long fields.

--- src/main/scala/crime.scala
+++ src/main/scala/crime.scala
@@ -6,16 +6,16 @@ object start {
     def readCrimes(fileName: String): Seq[Crime]
     def readAllCSVFiles(): Seq[Crime]
     def getListOfFiles(): List[String]
-    def topN(n: Int): Seq[(String, Seq[start.Crime])]
+    def topN(n: Int): Seq[((String, String), Seq[start.Crime])]
   }
-  case class Crime(coordinate: String, crimeType: String)
+  case class Crime(long: String, lat: String, crimeType: String)
 
   class CrimeCSVReader(val dir: String) extends CrimesReader {
     override def readCrimes(fileName: String): Seq[Crime] = {
       for {
         line <- Source.fromFile(fileName).getLines().drop(1).toVector
         values = line.split(",").map(_.trim) if values(0).nonEmpty && values(4).nonEmpty && values(5).nonEmpty
-      } yield Crime(s"${values(4)}, ${values(5)}", values(9))
+      } yield Crime(values(4), values(5), values(9))
     }
     override def readAllCSVFiles(): Seq[Crime] = {
       getListOfFiles().flatten(readCrimes).toVector
@@ -23,15 +23,15 @@ object start {
     override def getListOfFiles(): List[String] = {
       new File(dir).listFiles.filter(_.isFile).toList.filter(_.getName.endsWith("csv")).map(_.getAbsolutePath)
     }
-    override def topN(n: Int): Seq[(String, Seq[start.Crime])] = {
-      readAllCSVFiles().groupBy(_.coordinate).toSeq.sortBy(_._2.size).reverse.slice(0, n)
+    override def topN(n: Int): Seq[((String, String), Seq[start.Crime])] = {
+      readAllCSVFiles().groupBy(row => (row.long, row.lat)).toSeq.sortBy(-_._2.size).slice(0, n)
     }
   }
 
-  def printResult(row: (String, scala.Seq[start.Crime])): Any = {
+  def printResult(row: ((String, String), scala.Seq[start.Crime])): Any = {
     val thefts = row._2.map(_.crimeType).filter("(T|t)heft".r.findFirstIn(_).isDefined).distinct
     println("----------------------------")
-    println(s"(${row._1}): ${row._2.size}")
+    println(s"${row._1}: ${row._2.size}")
     println("Thefts:")
     thefts.foreach(println)
   }
--- src/test/scala/CsvReadTest.scala
+++ src/test/scala/CsvReadTest.scala
@@ -7,7 +7,9 @@ class CsvReadTest extends FunSuite {
     val path = System.getProperty("user.dir") + "/src/test/scala/test.csv"
     val res = new CrimeCSVReader(path).readCrimes(path)
     assert(res.size == 9385)
-    assert(res(0).coordinate.contains("-2.509126, 51.416137"))
-    assert(res(1).coordinate.contains("-2.514442, 51.409343"))
+    assert(res(0).lat.contains("51.416137"))
+    assert(res(0).long.contains("-2.509126"))
+    assert(res(1).lat.contains("51.409343"))
+    assert(res(1).long.contains("-2.514442"))
   }
 }
